9f52a43fb84d2cd12f001ed7f76bd8d9b7405688
RIKI PLAZA <rikiplaza1@gmail.com>
2022-09-19 18:25:56 -0400

    rough model work done

--- bot_db/models.py
+++ bot_db/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import jsonfield
 
 # Create your models here.
+
+class Member(models.Model):
+    guild = models.IntegerField(required=True)
+    hours = jsonfield.JSONField(required=True)
+    name = models.CharField(max_length=256, required=True)
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    guild = models.IntegerField(required=True)
+    title = models.CharField(max_length=256, required=True)
+    description = models.TextField(required=True)
+    time = models.DateTimeField(required=True)
+
