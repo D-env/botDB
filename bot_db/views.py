f5d5afe9f408a9dd6f4344ba39d55ba3a65d4a7a
RIKI PLAZA <rikiplaza1@gmail.com>
2022-09-19 19:38:32 -0400

    worked on settings, and views, created serializer

--- bot_db/views.py
+++ bot_db/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from .serializers import MemberSerializer, EventSerializer
+from bot_db.models import Event, Member
+from rest_framework import generics
 
 # Create your views here.
+class MemberList(generics.ListCreateAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+class MemberDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+
+class EventList(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
