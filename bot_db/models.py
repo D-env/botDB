55c1bf39c518b6305997dc3d0889d21e2090a660
Matthew Larkin <matthewlarkin8@gmail.com>
2022-09-21 16:39:29 -0400

    Updated models to match new database schema.

--- bot_db/models.py
+++ bot_db/models.py
@@ -1,20 +1,18 @@
 from django.db import models
 
-
-# Create your models here.
-
 class Member(models.Model):
-    guild = models.IntegerField()
-    hours = models.JSONField()
     name = models.CharField(max_length=256)
+    guild = models.IntegerField()
+    availability = models.JSONField()
     def __str__(self):
         return self.name
 
 
 class Event(models.Model):
-    guild = models.IntegerField()
-    title = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
     description = models.TextField()
-    time = models.DateTimeField()
-
-
+    duration = models.IntegerField()
+    guild = models.IntegerField()
+    start_time = models.IntegerField()
+    end_time = models.IntegerField()
+    day = models.CharField()
